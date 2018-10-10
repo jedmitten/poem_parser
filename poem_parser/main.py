1de17d19bd2b1b9a2835f8c594ae10422d75f2c9
jedmitten <jmitten@obsidiansecurity.com>
2018-10-09 22:01:20 -0700

    finish poetryminute.org print as json

--- poem_parser/main.py
+++ poem_parser/main.py
@@ -1,3 +1,4 @@
+import json
 import click
 
 from poem_parser import poetryminute as pm
@@ -5,7 +6,8 @@ from poem_parser import poetryminute as pm
 
 @click.command()
 def main():
-    pm.get_page_by_dir(pm.DATE)
+    poetry_minute = pm.get_poems()
+    print(json.dumps(poetry_minute, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
--- poem_parser/poetryminute.py
+++ poem_parser/poetryminute.py
@@ -6,43 +6,51 @@ AUTHOR = 'author'
 DATE = 'date'
 CATEGORY = 'category'
 TITLE = 'title'
-BASE_DIR = 'poems-by-'
+BASE_DIR = 'poems-by'
 DIRS = {
     AUTHOR: '-'.join([BASE_DIR, AUTHOR]),
     DATE: '-'.join([BASE_DIR, DATE]),
-    CATEGORY: '-'.join([BASE_DIR, CATEGORY]),
+    # CATEGORY: '-'.join([BASE_DIR, CATEGORY]),
     TITLE: '-'.join([BASE_DIR, TITLE])
 }
 BLOG_DIV = 'blog_div_text'
 
 
-def get_page_by_dir(dir):
-    full_url = '/'.join([BASE_URL, DIRS[dir]])
-    resp = requests.get(full_url)
-    soup = BeautifulSoup(resp.content, 'html.parser')
-    div = soup.find(attrs={"class": BLOG_DIV}).children
+def get_poems():
     collection = {}
-    month = ''
-    l_poems = []
-    for child in div:
-        if child.name == 'h2':
-            month = child.string
-        elif child.name == 'ul':
-            for li in child.find_all('li'):
-                l_poems = collection.get(month, [])
-                link = li.a['href']
-                text = li.string
-                text = text.replace('\xa0', ' ')
-                title, author = text.rsplit(' by ', 1)
-                d = {
-                    'link': link,
-                    'title': title,
-                    'author': author,
-                }
-                l_poems.append(d)
-                collection[month] = l_poems
-        # elif child.next == 'h2':
-        #     collection[month] = d
-        else:
-            continue
-    return l_poems
+    for poem_dir in DIRS:
+        full_url = '/'.join([BASE_URL, DIRS[poem_dir]])
+        resp = requests.get(full_url)
+        soup = BeautifulSoup(resp.content, 'html.parser')
+        div = soup.find(attrs={"class": BLOG_DIV}).children
+        category = ''
+        d_category = {}
+        for child in div:
+            if child.name == 'h2':
+                category = child.string
+            elif child.name == 'ul':
+                for li in child.find_all('li'):
+                    l_poems = d_category.get(category, [])
+                    link = li.a['href']
+                    text = li.string or li.text
+                    text = text.replace('\xa0', ' ')
+                    if ' by ' in text:
+                        title, author = text.rsplit(' by ', 1)
+                    else:
+                        title = text
+                    author = ''
+                    if poem_dir == AUTHOR:
+                        author = category
+                    d = {
+                        'link': link,
+                        'title': title,
+                        'author': author,
+                    }
+                    l_poems.append(d)
+                    d_category[category] = l_poems
+            # elif child.next == 'h2':
+            #     collection[month] = d
+            else:
+                continue
+        collection[poem_dir] = d_category
+    return collection
